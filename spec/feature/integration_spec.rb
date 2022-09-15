950e8d1f4bd1a2c5850cce87fba55dcad7e52beb
amcgrath17 <amcgrath17@tamu.edu>
2022-09-15 13:24:17 +0000

    Added spec files

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,33 @@ RSpec.describe 'Creating a book', type: :feature do
         expect(page).to have_content('harry potter')
     end
 end
+
+RSpec.describe 'Creating a book', type: :feature do
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in 'Author', with: 'rick riordan'
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('rick riordan')
+    end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in 'Price', with: 15
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content(15)
+    end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in 'Published Date', with: '2000-11-23'
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('2000-11-23')
+    end
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,55 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: 'rick riordan')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(price: 15)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(published: '2000-11-23')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a published date' do
+    subject.published = nil
+    expect(subject).not_to be_valid
+  end
+end
+
